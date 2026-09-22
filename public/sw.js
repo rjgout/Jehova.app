8cfafe182838121528a9f27d74373f0f65cf9e86
JonnyTheG <59642233+rjgout@users.noreply.github.com>
2026-09-22 15:48:50 +0200

    Wacht op tonen en badge bij pushverwerking

--- public/sw.js
+++ public/sw.js
@@ -19,13 +19,15 @@ self.addEventListener("push", (event) => {
   }
 
   event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      data: { url: data.url },
-    }),
-    typeof data.badge === "number" && data.badge > 0
-      ? self.registration.setAppBadge(data.badge).catch(() => {})
-      : Promise.resolve()
+    Promise.all([
+      self.registration.showNotification(data.title, {
+        body: data.body,
+        data: { url: data.url },
+      }),
+      typeof data.badge === "number" && data.badge > 0
+        ? self.registration.setAppBadge(data.badge).catch(() => {})
+        : Promise.resolve(),
+    ])
   );
 });
 
